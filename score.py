88e344bd57bb97cb1aa061c261e372bb7e9ea2e8
PeacePeters <68206315+PeacePeters@users.noreply.github.com>
2021-02-28 16:56:24 +0100

    Update score.py

--- score.py
+++ score.py
@@ -6,7 +6,7 @@ import numpy as np
 from azureml.core import Model
 from sklearn.externals import joblib
 
-test_data_sample=pd.DataFrame(data=[{"Age":75, "anaemia":0,"creatinine_phosphokinase":582,"diabetes":0, "ejection_fraction":20, "high_blood_pressure":1, "platelets":265000, "serum_creatinine":1.9, "serum_sodium":130, "sex":1, "smoking":0, "time":4}])
+test_data_sample=pd.DataFrame(data=[{"Age":75, "anaemia":0,"creatinine_phosphokinase":582,"diabetes":0, "ejection_fraction":20, "high_blood_pressure":1, "platelets":265000, "serum_creatinine":1.9, "serum_sodium":130, "sex":1, "smoking":0}])
 
 input_sample = pd.DataFrame({"Age": pd.Series([0], dtype="float64"), "anaemia": pd.Series([0], dtype="int64"), "creatinine_phosphokinase": pd.Series([0], dtype="int64"), "diabetes": pd.Series([0], dtype="int64"), "ejection_fraction": pd.Series([0], dtype="int64"), "high_blood_pressure": pd.Series([0], dtype="int64"), "platelets": pd.Series([0], dtype="float64"), "serum_sodium": pd.Series([0], dtype="int64"), "sex": pd.Series([0], dtype="int64"), "smoking": pd.Series([0], dtype="int64")})
 output_sample = np.array([0])
