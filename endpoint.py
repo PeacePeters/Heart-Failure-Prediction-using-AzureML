b2625f0a8db88881c86a0c75cb8fb28f7d993a74
PeacePeters <68206315+PeacePeters@users.noreply.github.com>
2021-04-14 23:37:06 +0100

    Update endpoint.py

--- endpoint.py
+++ endpoint.py
@@ -17,11 +17,6 @@ def allowSelfSignedHttps(allowed):
 
 allowSelfSignedHttps(True) # this line is needed if you use self-signed certificate in your scoring service.
 
-# URL for the web service
-scoring_uri = 'http://e5bf398f-b130-49f5-893d-8e3c729d8bcb.southcentralus.azurecontainer.io/score'
-# If the service is authenticated, set the key or token
-key = '1fyaYzZZb4v0ouH1azj3iPksgmwxtjur'
-
 # Two sets of data to score, so we get two results back
 data = {"data":
         [
@@ -49,38 +44,18 @@ data = {"data":
             "serum_creatinine":0,
             "serum_sodium":138,
             "sex":0,
-            "smoking":0,
-          },
+            "smoking":0
+          }
         ]
     }
 
 body = str.encode(json.dumps(data))
 
-# URL for the web service
-#url = 'http://5af42fb2-502b-4281-8a72-64f5722f362f.southcentralus.azurecontainer.io/score'
-# If the service is authenticated, set the key or token
-#api_key = '' # Replace this with the API key for the web service
-# Set the content type and authorization header if authentication is enabled
-#headers = {'Content-Type':'application/json', 'Authorization':('Bearer '+ api_key)}
+url = 'http://5af42fb2-502b-4281-8a72-64f5722f362f.southcentralus.azurecontainer.io/score'
+api_key = '' # Replace this with the API key for the web service
+headers = {'Content-Type':'application/json', 'Authorization':('Bearer '+ api_key)}
 
-#req = urllib.request.Request(url, body, headers)
-
-# Convert to JSON string
-input_data = json.dumps(data)
-with open("data.json", "w") as _f:
-    _f.write(input_data)
-
-# Set the content type
-headers = {'Content-Type': 'application/json'}
-# If authentication is enabled, set the authorization header
-headers['Authorization'] = f'Bearer {key}'
-
-# Make the request and display the response
-#resp = requests.post(scoring_uri, input_data, headers=headers)
-#print(resp.json())
-
-# Make the request and display the response
-req = urllib.request.Request(scoring_url, input_data, headers)
+req = urllib.request.Request(url, body, headers)
 
 try:
     response = urllib.request.urlopen(req)
